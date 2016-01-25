c3311428118817ab7c2a515bf6d2d719d7fc7a4e
Joseph Southan <joseph+git@southanuk.co.uk>
2016-01-25 23:05:23 +0000

    Don't append migrations

--- lib/payola/engine.rb
+++ lib/payola/engine.rb
@@ -10,14 +10,6 @@ module Payola
       g.helper false
     end
 
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
-
     initializer :inject_helpers do |app|
       ActiveSupport.on_load :action_controller do
         ::ActionController::Base.send(:helper, Payola::PriceHelper)
